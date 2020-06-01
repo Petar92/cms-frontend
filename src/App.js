295302e2ee87e90542813ef8f4cf6e9262a1f46b
repic <repic92@gmail.com>
2020-06-01 10:24:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Image from './components/Image'
+import Focus from './components/Focus'
+import News from './components/News'
+import Links from './components/Links'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div >
-      
+      <Header />
+      <Menu />
+      <Image />
+      <Focus />
+      <News />
+      <Links />
+      <Footer />
     </div>
   );
 }
